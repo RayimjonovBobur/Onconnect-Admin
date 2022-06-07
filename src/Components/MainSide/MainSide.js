111cee7dad429cb097be43bdcedd2ec54431b235
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-06-07 11:38:30 +0500

    fds

--- src/Components/MainSide/MainSide.js
+++ src/Components/MainSide/MainSide.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MainSide.scss";
+import { MainSideData } from "./MainSideData";
 
 const MainSide = () => {
   return (
@@ -30,7 +31,39 @@ const MainSide = () => {
               <p className="text-xs font-normal">Манфаатдор идора</p>
               <h6 className="font-semibold text-sm mt-3">Идоранинг номи</h6>
             </div>
-            <button className="add-data text-xs ml-7 mt-4   ">Тулик куриш</button>
+            <button className="add-data text-xs ml-7 mt-4   ">
+              Тулик куриш
+            </button>
+          </div>
+        </div>
+        <div className="petition-content">
+          <div className="petition-data">
+            <h2 className="text-lg font-medium mainside-title  mt-2 mb-3">
+              Ариза (Шикоят) мазмуни
+            </h2>
+            <hr className="petition-line" />
+            <div className="petition-header font-medium text-sm mt-3 ">
+              <div className="p-3 flex">
+                <span className="mr-2">№</span>
+                <p className="">Иш туркуми</p>
+                <p className="">Кушимча иш туркуми</p>
+                <p className="">Иккиламчи иш туркуми</p>
+              </div>
+            </div>
+            <div className="petition-data-content">
+              {MainSideData.map((item, i) => (
+                <div className="petition-data-content-item">
+                  <div className="flex">
+                    <span>1</span>
+                    <div className="flex">
+                      <h4>{item.firstTitle}</h4>
+                      <h4>{item.secondTitle}</h4>
+                      <h4>{item.thirdTitle}</h4>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
